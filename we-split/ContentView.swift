cad00f76ecc1852a9a28effefde902d62148b7e3
vcosmin2701 <v.cosmin2717@gmail.com>
2024-09-22 17:07:30 +0100

    [learn] - two-way data binding example

--- we-split/ContentView.swift
+++ we-split/ContentView.swift
@@ -8,14 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
-    
+    @State private var name = ""
     var body: some View {
-        Button("Tap Count: \(tapCount)"){
-            tapCount += 1
-        }
-        Button("Reset count"){
-            tapCount = 0
+        Form {
+            TextField("Enter your name", text: $name)
+            Text("Hello, \(name)")
         }
     }
 }
