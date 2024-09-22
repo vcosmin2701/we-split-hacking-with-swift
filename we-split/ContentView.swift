e6928b943d392e971f0df7c3a71ebe8f584be906
vcosmin2701 <v.cosmin2717@gmail.com>
2024-09-22 16:39:49 +0100

    [learn] - basic form, group and section

--- we-split/ContentView.swift
+++ we-split/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Group {
+                Text("Hello World!")
+            }
+            
+            Section {
+                Text("Hello from section!")
+            }
         }
-        .padding()
     }
 }
 
